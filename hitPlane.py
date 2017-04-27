26c3183e67624326ac5642205743c0e31abe2624
DamienXiong <2454471251@qq.com>
2017-04-27 21:54:33 +0800

    use class to add bullet

--- hitPlane.py
+++ hitPlane.py
@@ -1,27 +1,37 @@
 # -*- coding: utf-8 -*-
 import pygame
 from sys import exit
+
+class Bullet(object):
+    def __init__(self):
+        self.x = 0
+        self.y = -1
+        self.image = pygame.image.load('bullet.png').convert_alpha()
+
+    def move(self):
+        if self.y < 0:
+            mouseX, mouseY = pygame.mouse.get_pos()
+            self.x = mouseX - self.image.get_width() / 2
+            self.y = mouseY - self.image.get_height() / 2
+        else:
+            self.y -= 5
+
 pygame.init()
 screen = pygame.display.set_mode((450, 800), 0, 32)
 pygame.display.set_caption('hello, world!')
 background = pygame.image.load('bg.jpg').convert_alpha()
 plane = pygame.image.load('plane.png').convert_alpha()
-bullet = pygame.image.load('bullet.png').convert_alpha()
-bullet_x = 0
-bullet_y = -1
+bullet = Bullet()
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
     screen.blit(background, (0, 0))
+    bullet.move()
+    screen.blit(bullet.image, (bullet.x, bullet.y))
     x, y = pygame.mouse.get_pos()
-    if bullet_y < 0:
-        bullet_x = x - bullet.get_width() / 2
-        bullet_y = y - bullet.get_height() / 2
-    else:
-        bullet_y -= 5
-    screen.blit(bullet, (bullet_x, bullet_y))
     x -= plane.get_width() / 2
     y -= plane.get_height() / 2
     screen.blit(plane, (x, y))
