2f8b168853779afacb266de2209e0f815bca6249
DamienXiong <2454471251@qq.com>
2017-04-27 22:03:37 +0800

    add enemy

--- hitPlane.py
+++ hitPlane.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import pygame
+import random
 from sys import exit
 
 class Bullet(object):
@@ -16,12 +17,29 @@ class Bullet(object):
         else:
             self.y -= 5
 
+class Enemy(object):
+    def restart(self):
+        self.x = random.randint(50, 400)
+        self.y = random.randint(-200, -50)
+        self.speed = random.random() + 0.1
+
+    def __init__(self):
+        self.restart()
+        self.image = pygame.image.load('enemy.png').convert_alpha()
+
+    def move(self):
+        if self.y < 800:
+            self.y += self.speed
+        else:
+            self.restart()
+
 pygame.init()
 screen = pygame.display.set_mode((450, 800), 0, 32)
 pygame.display.set_caption('hello, world!')
 background = pygame.image.load('bg.jpg').convert_alpha()
 plane = pygame.image.load('plane.png').convert_alpha()
 bullet = Bullet()
+enemy = Enemy()
 
 while True:
     for event in pygame.event.get():
@@ -31,6 +49,8 @@ while True:
     screen.blit(background, (0, 0))
     bullet.move()
     screen.blit(bullet.image, (bullet.x, bullet.y))
+    enemy.move()
+    screen.blit(enemy.image, (enemy.x, enemy.y))
     x, y = pygame.mouse.get_pos()
     x -= plane.get_width() / 2
     y -= plane.get_height() / 2
