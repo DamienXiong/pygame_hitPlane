f7a3215331234fcff4466d8c078fe19a43aa8c45
DamienXiong <2454471251@qq.com>
2017-04-27 21:37:34 +0800

    add plane

--- hitPlane.py
+++ hitPlane.py
@@ -5,18 +5,15 @@ pygame.init()
 screen = pygame.display.set_mode((600, 300), 0, 32)
 pygame.display.set_caption('hello, world!')
 background = pygame.image.load('background.jpg').convert()
-image = ['background.jpg', 'background2.jpg']
-mouseclick = 0
+plane = pygame.image.load('plane.png').convert()
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if mouseclick % 2 == 0:
-                background = pygame.image.load(image[1]).convert()
-            else:
-                background = pygame.image.load(image[0]).convert()
-            mouseclick += 1
     screen.blit(background, (0, 0))
+    x, y = pygame.mouse.get_pos()
+    x -= plane.get_width() / 2
+    y -= plane.get_height() / 2
+    screen.blit(plane, (x, y))
     pygame.display.update()
