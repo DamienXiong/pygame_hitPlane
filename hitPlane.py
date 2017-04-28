fe8bc5cce9f99e141bc884e7ee4352bae86a862f
DamienXiong <2454471251@qq.com>
2017-04-28 08:48:12 +0800

    many bullets

--- hitPlane.py
+++ hitPlane.py
@@ -8,14 +8,19 @@ class Bullet(object):
         self.x = 0
         self.y = -1
         self.image = pygame.image.load('bullet.png').convert_alpha()
+        self.active = False
 
     def move(self):
+        if self.active:
+            self.y -= 3
         if self.y < 0:
-            mouseX, mouseY = pygame.mouse.get_pos()
-            self.x = mouseX - self.image.get_width() / 2
-            self.y = mouseY - self.image.get_height() / 2
-        else:
-            self.y -= 5
+            self.active = False
+
+    def restart(self):
+        mouseX, mouseY = pygame.mouse.get_pos()
+        self.x = mouseX - self.image.get_width() / 2
+        self.y = mouseY - self.image.get_height() / 2
+        self.active = True
 
 class Enemy(object):
     def restart(self):
@@ -38,7 +43,12 @@ screen = pygame.display.set_mode((450, 800), 0, 32)
 pygame.display.set_caption('hello, world!')
 background = pygame.image.load('bg.jpg').convert_alpha()
 plane = pygame.image.load('plane.png').convert_alpha()
-bullet = Bullet()
+bullets = []
+for i in range(5):
+    bullets.append(Bullet())
+count_b = len(bullets)
+index_b = 0
+interval_b = 0
 enemy = Enemy()
 
 while True:
@@ -47,8 +57,15 @@ while True:
             pygame.quit()
             exit()
     screen.blit(background, (0, 0))
-    bullet.move()
-    screen.blit(bullet.image, (bullet.x, bullet.y))
+    interval_b -= 1
+    if interval_b < 0:
+        bullets[index_b].restart()
+        interval_b = 100
+        index_b = (index_b + 1) % count_b
+    for b in bullets:
+        if b.active:
+            b.move()
+            screen.blit(b.image, (b.x, b.y))
     enemy.move()
     screen.blit(enemy.image, (enemy.x, enemy.y))
     x, y = pygame.mouse.get_pos()
